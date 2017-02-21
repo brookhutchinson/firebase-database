b4141ab132832570e94075bb29590de06533a2b7
Brook Hutchinson <brookhutchinson@outlook.com>
2017-02-21 09:59:27 -0600

    Create Firebase Configuration File named "firebase.config.ts" inside folder "src/environments", Define const named "fiebaseConfig" inside "firebase.config.ts", Define firebase database configuration properties inside Firebase Configuration File

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmUY8jLTa6b4oJjSeL70q-6GTReSB2was",
+  authDomain: "fir-a6c47.firebaseapp.com",
+  databaseURL: "https://fir-a6c47.firebaseio.com",
+  storageBucket: "fir-a6c47.appspot.com"
+}
