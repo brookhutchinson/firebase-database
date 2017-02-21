ff421563cffa77c8efade368bf85a44218817fdf
Brook Hutchinson <brookhutchinson@outlook.com>
2017-02-21 10:13:52 -0600

    Setup connection between Angular App and the Angular Firebase Project, Import Service named "AngularFireModule" into app.module.ts, Import Firebase Configuration Object from firebase.config.ts into app.module.ts, Add AngularFireModule to the @NgModule Decorator "imports" array inside app.module.ts, Configure the AngularFireModule using "firebaseConfig" Object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 // modules
-import { NgModule }        from '@angular/core';
-import { BrowserModule }   from '@angular/platform-browser';
-import { FormsModule }     from '@angular/forms';
-import { HttpModule }      from '@angular/http';
+import { NgModule }          from '@angular/core';
+import { BrowserModule }     from '@angular/platform-browser';
+import { FormsModule }       from '@angular/forms';
+import { HttpModule }        from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 // components
-import { AppComponent }    from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent }   from './components/home/home.component';
+import { AppComponent }      from './app.component';
+import { NavbarComponent }   from './components/navbar/navbar.component';
+import { HomeComponent }     from './components/home/home.component';
+
+// firebase config
+import { firebaseConfig }    from './../environments/firebase.config';
 
 @NgModule({
   // modules
-  imports: [ BrowserModule, FormsModule, HttpModule ],
+  imports: [ BrowserModule, FormsModule, HttpModule, AngularFireModule.initializeApp(firebaseConfig) ],
   // components
   declarations: [ AppComponent, NavbarComponent, HomeComponent ],
   // services
