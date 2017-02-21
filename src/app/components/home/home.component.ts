c22a031cde1a058bc75ab4e248b985fabaeb16c4
Brook Hutchinson <brookhutchinson@outlook.com>
2017-02-21 10:46:49 -0600

    Setup HomeComponent to use AngularFire Service, Import AngularFire Service and FirebaseListObservable from package library named "angularfire2" into home.component.ts, Define constructor() function inside HomeComponent Class, Inject AngularFire Service into HomeComponent Class by defining a property parameter inside the constructor() function named "af" of type "AngularFire"

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+// components
+import { Component }                           from '@angular/core';
+
+// firebase
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private af: AngularFire) {
+
+  }
+}
